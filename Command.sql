baeb567ede346ba1fe3a7f10d7faaab623dbd4b2
102914174-AG <61397195+102914174-AG@users.noreply.github.com>
2021-05-10 10:28:05 +1000

    Task1
    Task 1 Complete

--- Command.sql
+++ Command.sql
@@ -1,1 +1,16 @@
+/*Addison Gale
+102914174*/
 
+/* ERD to Rational Schema*/
+/*Tour(TourName, Description)
+PK(Tourname)
+
+Event(EventYear, EventMonth, EventDay, Fee, TourName)
+PK(EventYear, EventMonth, EventDay)
+FK(Tourname)
+
+Booking(DateBooked, Payment, ClientID, Tourname, EventYear, EventMonth, EventDay)
+FK(ClientID, Tourname, EventYear, EventMonth, EventDay)
+
+Client(ClientID, Surname, GivenName, Gender)
+PK(ClientID)
